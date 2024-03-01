8afa192d232d821e4f9ea0a046ee30a6c16b76f8
alain <alain8864@gmail.com>
2024-03-01 22:43:25 +0100

    pruebas

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import io.quarkus.runtime.Quarkus;
-import io.quarkus.runtime.annotations.QuarkusMain;
-
-@QuarkusMain
-public class Main {
-    public static void main(String...args){
-        Quarkus.run(args);
-    }
-}
